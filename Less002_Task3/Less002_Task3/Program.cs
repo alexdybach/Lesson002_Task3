a5b3165454d02abe1fcde71b55b8f9873b36e7d0
Олександр Дибач <alex.dybach.2014@i.ua>
2020-10-27 13:58:55 +0200

    Done lesson 2 task 3

---
+++ Less002_Task3/Less002_Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Less002_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 5;
+            int myVar2 = 7;
+
+            int addition = myVar1 + myVar2;
+            int subtruction = myVar1 - myVar2;
+            double division = (double) myVar1 / myVar2;
+            int multiplication = myVar1 * myVar2;
+            int percentage = myVar1 % myVar2;
+
+            Console.WriteLine($"myVar1 + myVar2 = {addition} \n myVar1 - myVar2 = {subtruction} \n myVar1 / myVar2 = {division} \n myVar1 * myVar2 = {multiplication} \n myVar1 % myVar2 = {percentage}");
+        }
+    }
+}
